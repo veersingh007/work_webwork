f7775e128b9f49f45ef1d639156d2c3f022bf831
veersingh007 <veer.exe@gmail.com>
2025-12-09 01:46:39 +0530

    first_try

--- veer_app/src/app/page.js
+++ veer_app/src/app/page.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -16,6 +17,9 @@ export default function Home() {
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             To get started, edit the page.js file.
           </h1>
+          <Link href="/Next_page">
+          <h1>next_page</h1>
+          </Link>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Looking for a starting point or more instructions? Head over to{" "}
             <a
